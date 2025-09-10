c05c11cb1511a62315ab788f869ee1ce04c57ca2
Yi leng Yao <yilengyao@gmail.com>
2025-09-09 21:25:04 -0700

    add unit number to address

--- src/models/address.ts
+++ src/models/address.ts
@@ -4,13 +4,14 @@ type LatLng = {
 };
 
 interface Address {
-  placeId: string;           // place_id (for Google Places)
-  name?: string;             // place name / establishment name (use instead of formatted address)
-  city?: string;             // locality / postal_town
-  province?: string;         // full name British Columbia
-  country?: string;          // ISO alpha-2 (e.g. "CA") - preferred
-  postalCode?: string;       // postal_code
-  location?: LatLng;         // lat/lng
+    placeId: string;           // place_id (for Google Places)
+    name?: string;             // place name / establishment name (use instead of formatted address)
+    unitNumber?: string;       // Unit, suite or apartment (subpremise)
+    city?: string;             // locality / postal_town
+    province?: string;         // full name British Columbia
+    country?: string;          // ISO alpha-2 (e.g. "CA") - preferred
+    postalCode?: string;       // postal_code
+    location?: LatLng;         // lat/lng
 };
 
 export {
