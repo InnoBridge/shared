bc0b0aae66faa66aa0f64d08d06e001097dcc377
Yi leng Yao <yilengyao@gmail.com>
2025-09-10 21:06:48 -0700

    split address to baseAddress for registration and address for backend storage

--- src/models/address.ts
+++ src/models/address.ts
@@ -3,8 +3,8 @@ type LatLng = {
     lng: number;
 };
 
-interface Address {
-    id: string;            // internal ID
+// Derived from Google Places API, to save on Clerk for user registration
+interface BaseAddress {
     placeId: string;           // place_id (for Google Places)
     name?: string;             // place name / establishment name (use instead of formatted address)
     unitNumber?: string;       // Unit, suite or apartment (subpremise)
@@ -13,6 +13,12 @@ interface Address {
     country?: string;          // ISO alpha-2 (e.g. "CA") - preferred
     postalCode?: string;       // postal_code
     location?: LatLng;         // lat/lng
+};
+
+// Backend storage representation of an address
+interface Address extends BaseAddress {
+    id: string;                // internal ID
+    userId?: string;           // user ID (if associated with a user)
 };
 
 export {
