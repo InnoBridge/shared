27a2a026a11b87c5c374698f6bf003297a571c13
Yi leng Yao <yilengyao@gmail.com>
2025-09-14 10:22:38 -0700

    address pr comment

--- src/models/provider.ts
+++ src/models/provider.ts
@@ -10,5 +10,5 @@ export interface Provider extends User {
   serviceRadius: number;
   canVisitClientHome: boolean;
   virtualHelpOffered: boolean;
-  businessName?: string | null;
+  businessName?: string;
 }
